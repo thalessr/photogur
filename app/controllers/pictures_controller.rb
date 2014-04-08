a30dac6f3c5fab66e948dc12928a3b62dcd55b5b
Thales Ribeiro <thalessr@gmail.com>
2014-04-08 15:48:41 -0400

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,44 +1,10 @@
 class PicturesController < ApplicationController
 	def index
-		# fake
-		@pictures = [
-	      {
-	        :title  => "The old church on the coast of White sea",
-	        :artist => "Sergey Ershov",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	      },
-	      {
-	        :title  => "Sea Power",
-	        :artist => "Stephen Scullion",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	      },
-	      {
-	        :title  => "Into the Poppies",
-	        :artist => "John Wilhelm",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	      }
-	    ]
+		@pictures = Picture.all
 	end
 	
 	def show
-		@pictures = [
-	      {
-	        :title  => "The old church on the coast of White sea",
-	        :artist => "Sergey Ershov",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	      },
-	      {
-	        :title  => "Sea Power",
-	        :artist => "Stephen Scullion",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	      },
-	      {
-	        :title  => "Into the Poppies",
-	        :artist => "John Wilhelm",
-	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	      }
-	    ]
-	    @picture = @pictures[params[:id].to_i]
+	    @picture = @pictures[params[:id]]
 	end
 
 	def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Picture.create!(
+    :title  => "The old church on the coast of White sea",
+    :artist => "Sergey Ershov",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+  )
+
+Picture.create!(
+    :title  => "Sea Power",
+    :artist => "Stephen Scullion",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"  
+  )
+
+Picture.create!(
+    :title  => "Into the Poppies",
+    :artist => "John Wilhelm",
+    :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"  
+  )
