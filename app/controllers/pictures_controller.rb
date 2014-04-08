0c70061817fafbf648bbfcd04454ff01895242b1
Thales Ribeiro <thalessr@gmail.com>
2014-04-08 14:31:37 -0400

    Making pictures show at /pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,22 @@
 class PicturesController < ApplicationController
 	def index
+		# fake
+		@pictures = [
+	      {
+	        :title  => "The old church on the coast of White sea",
+	        :artist => "Sergey Ershov",
+	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+	      },
+	      {
+	        :title  => "Sea Power",
+	        :artist => "Stephen Scullion",
+	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+	      },
+	      {
+	        :title  => "Into the Poppies",
+	        :artist => "John Wilhelm",
+	        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+	      }
+	    ]
 	end
 end
