78e8af9ad6bd4965b7738300bbf3b712c1eb1d20
Thales Ribeiro <thalessr@gmail.com>
2014-04-08 16:42:19 -0400

    Adding feature to edit pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -20,6 +20,19 @@ class PicturesController < ApplicationController
 		end
 	end
 
+	def edit
+		@picture = Picture.find(params[:id])
+	end
+
+	def update
+		@picture = Picture.find(params[:id])
+		if @picture.update_attributes(picture_params)
+			redirect_to @picture
+		else
+			render :edit
+		end
+	end
+
 	private
 	def picture_params
 		params.require(:picture).permit(:artist, :title, :url)
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Photogur::Application.routes.draw do
 
   get 'pictures/:id'  => 'pictures#show', as: 'picture'
 
+  get 'pictures/:id/edit' => 'pictures#edit', as: 'edit_picture'
+  patch 'pictures/:id' => 'pictures#update'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
