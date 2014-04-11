2a4391c59e86f463c661f5769523b1957db4c71f
Thales Ribeiro <thalessr@gmail.com>
2014-04-11 18:09:49 -0400

    Display most recent pictures on the index page

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,5 @@
 class Picture < ActiveRecord::Base
+	scope :newest_first, -> {order("created_at DESC ")}
+	scope :most_recent_five, -> {newest_first.limit(5)}
+	scope :created_before, ->(time) { where "created_at < ?", time}
 end
